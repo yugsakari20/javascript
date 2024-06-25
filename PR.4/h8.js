c7c702f0e185567e60d59bb820c6f779d6b4bb8a
yugsakari20 <yugsakariya976@gmail.com>
2024-06-25 20:00:22 +0530

    25/06/2024

--- PR.4/h8.js
+++ PR.4/h8.js
@@ -3,13 +3,12 @@ let a = 4;
 let b = 8;
 let c = 5;
 
-// First comparison: num1 > num2
-let firstComparison = a > b;
-console.log(firstComparison); // Output: false
 
-// Adding num3 to num1
+let firstComparison = a > b;
+console.log(firstComparison); 
+
+
 a += c;
 
-// Second comparison: num1 > num2 after adding num3 to num1
 let secondComparison = a > b;
-console.log(secondComparison); // Output: true
+console.log(secondComparison); 
